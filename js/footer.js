3b75493f07572e3473478d82c377f081468fdb45
DmytroZoteev <137426491+DmytroZoteev@users.noreply.github.com>
2023-08-16 16:06:15 +0000

    upd

--- js/footer.js
+++ js/footer.js
@@ -3,25 +3,7 @@ const getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
 const author = document.querySelector('.footer_text-author');
 
 const wordTeamInDifferentLanguages = [
-    '© Created by Team- 05',
-    '© Created by Команда- 05',
-    '© Created by Equipo - 05',
-    '© Created by Équipe - 05',
-    '© Created by Squadra - 05',
-    '© Created by 团队 - 05',
-    '© Created by チーム - 05',
-    '© Created by 팀 - 05',
-    '© Created by فريق -05 ',
-    '© Created by टीम - 05',
-    '© Created by Nhóm - 05',
-    '© Created by Tim - 05',
-    '© Created by Pasukan - 05',
-    '© Created by Koponan - 05',
-    '© Created by Ομάδα - 05',
-    '© Created by Lag - 05',
-    '© Created by Csapat - 05',
-    '© Created by Tým - 05',
-    '© Created by Echipă - 05',
+    '© Created by DZ',
 ];
 
 author.innerText = getRandom(wordTeamInDifferentLanguages);
